f80941e9d12cb04ed036ffb5f905cdf07f581349
avecenob <aryabasc@gmail.com>
2023-09-23 01:07:15 +0700

    update routes

--- routes.js
+++ routes.js
@@ -1,18 +1,39 @@
 const routes = [
     {
-        method: 'GET',
-        path: '/',
+        method: "GET",
+        path: "/",
         handler: (request, h) => {
-            return 'Homepage';
-        },
+            return "Homepage";
+        }
     },
     {
-        method: 'GET',
-        path: '/about',
+        method: "*",
+        path: "/",
         handler: (request, h) => {
-            return 'About page';
-        },
+            return h.response("Page cannot be accessed with that method").code(400);
+        }
     },
+    {
+        method: "GET",
+        path: "/about",
+        handler: (request, h) => {
+            return "About page";
+        }
+    },
+    {
+        method: "*",
+        path: "/about",
+        handler: (request, h) => {
+            return h.response("Page cannot be accessed with that method").code(400);
+        }
+    },
+    {
+        method: "*",
+        path: "/{any*}",
+        handler: (request, h) => {
+            return h.response("Page not found").code(404);
+        }
+    }
 ]
 
 module.exports = routes;
